e6ba03c1e60cbbafb62c56a9babd8aa3e5f498e1
hcfw007 <brotherstyx@gmail.com>
2021-01-25 19:28:27 +0800

    feat: serve basic static files

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Catchup Auto Update Test! ESLint installed!'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'index.html')))
+
+app.use(express.static('frontend'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
