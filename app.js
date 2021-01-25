27abbc0cd5c97c15164c1fed3c69e4eefdc779a3
hcfw007 <brotherstyx@gmail.com>
2021-01-25 13:56:05 +0800

    feat: import eslint

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Catchup Auto Update Test!'))
+app.get('/', (req, res) => res.send('Catchup Auto Update Test! ESLint installed!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
