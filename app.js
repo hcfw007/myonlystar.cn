a127a0e689eaa4f33b16b4ed99943eb1e1bdda23
NickWang <brotherstyx@gmail.com>
2021-01-27 18:32:43 +0800

    feat: 🎸 api test

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const path = require('path')
 const express = require('express')
+
+const apiRouter = require('./src/router')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'index.html')))
+
+app.use('/api', apiRouter)
 
 app.use(express.static('frontend'))
 
