03a8e5652ec924e54cb163dca2c960e44b0646be
NickWang <brotherstyx@gmail.com>
2021-02-06 12:28:49 +0800

    fix: 🐛 tag model name

--- src/db/dao/tag.js
+++ src/db/dao/tag.js
@@ -3,11 +3,19 @@ const mongoose = require('../db.js')
 const log = require('npmlog')
 const PRE = 'Tag DB'
 
-const userSchema = {
+const tagSchema = {
   name: String,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
 }
 
-const Tag = mongoose.model('tag', userSchema)
+const Tag = mongoose.model('tag', tagSchema)
 
 const findTag = (tagInfo) => {
   log.verbose(PRE, `findTag(${ JSON.stringify(tagInfo) })`)
@@ -21,7 +29,7 @@ const createTag = (tagInfo) => {
 
 const listTag = () => {
   log.verbose(PRE, 'getTagList()')
-  return Tag.findAll()
+  return Tag.find({ deleted: false }).sort('-date')
 }
 
 module.exports = {
