8a8826397a5c419d1dce08f4af1afd0a6bf6e1c6
NickWang <brotherstyx@gmail.com>
2021-02-06 12:23:46 +0800

    test: 💍 register tags controller in router

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 const blogController = require('./controller/blog.controller')
+const tagController = require('./controller/tag.controller')
 
 const express = require('express')
 const router = express.Router()
@@ -11,5 +12,10 @@ router.get('/test', (req, res) => {
 router.post('/blog/post', blogController.postBlog)
 router.get('/blog/list', blogController.listBlog)
 router.get('/blog/getById', blogController.getBlogById)
+
+// tags
+
+router.post('/tags/create', tagController.addTag)
+router.get('/tags/list', tagController.listTag)
 
 module.exports = router
