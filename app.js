b6e96afc3ce53471883ddad83d88c3775c01dad2
NickWang <brotherstyx@gmail.com>
2021-02-19 15:54:25 +0800

    feat: 🎸 allow cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
+const AppPort = require('./config').AppPort
 
 const log = require('npmlog')
 log.level = 'verbose'
@@ -7,10 +9,11 @@ log.level = 'verbose'
 const apiRouter = require('./src/router')
 
 const app = express()
-const port = 3000
+const port = AppPort
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cors())
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'index.html')))
 
