3fe8fcd2c539f4943d7c3cf5a76c0c450a8569d6
NickWang <brotherstyx@gmail.com>
2021-03-29 19:16:07 +0800

    feat: 🎸 log for db

--- src/db/db.js
+++ src/db/db.js
@@ -1,16 +1,19 @@
 const dbConfig = require('../../config')
 const mongoose = require('mongoose')
+const log = require('npmlog')
 
+const PRE = 'db'
 const url = `mongodb://${ dbConfig.MongoURL }:${ dbConfig.MongoPort }/${ dbConfig.MongoDbName }`
 
+log.info(PRE, `mongo url: ${ url }`)
 /**
  * 连接
  */
 mongoose.connect(url, { useNewUrlParser: true }, (err) => {
   if (err) {
-    console.log('Connection Error:' + err)
+    log.error(`Mongoose connection error ${err}`)
   } else {
-    console.log('Connection SUCCESS!')
+    log.info(PRE, 'mongoose connect')
   }
 })
 
@@ -18,21 +21,21 @@ mongoose.connect(url, { useNewUrlParser: true }, (err) => {
   * 连接SUCCESS
   */
 mongoose.connection.on('connected', function () {
-  console.log('Mongoose connection open to ' + url)
+  log.info(PRE, `Mongoose connection open to ${ url }`)
 })
 
 /**
  * 连接异常
  */
 mongoose.connection.on('error', function (err) {
-  console.log('Mongoose connection error: ' + err)
+  log.error(`Mongoose connection error ${ err }`)
 })
 
 /**
  * 连接断开
  */
 mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose connection disconnected')
+  log.warn('Mongoose connection disconnected')
 })
 
 module.exports = mongoose
