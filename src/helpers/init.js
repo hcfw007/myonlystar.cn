d658993fffb4bc0712811b13c20517b66b4baf97
NickWang <brotherstyx@gmail.com>
2021-04-06 19:44:17 +0800

    fix: 🐛 check if frontend exists when init

--- src/helpers/init.js
+++ src/helpers/init.js
@@ -1,10 +1,17 @@
 const projectService = require('../service/project.service')
 const log = require('npmlog')
+const fs = require('fs')
+const path = require('path')
 
 const PRE = 'init'
 
 const init = () => {
   log.verbose(PRE, 'init frontend')
+  const frontendExists = fs.existsSync(path.join(__dirname, '..', '..', 'frontend', 'index.html'))
+  if (frontendExists) {
+    log.info(PRE, 'frontend already exists')
+  }
+  log.info(PRE, 'frontend not found, will get latest version...')
   projectService.getLatestFrontend()
 }
 
